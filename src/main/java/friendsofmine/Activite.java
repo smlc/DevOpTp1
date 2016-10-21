5f80a76ed29e5f9f28215cb069f99e2d5402e92a
WalidBOULFRAD <walid.boulfrad@gmail.com>
2016-10-21 11:42:26 +0200

    Update Activite.java

--- src/main/java/friendsofmine/Activite.java
+++ src/main/java/friendsofmine/Activite.java
@@ -1,23 +1,25 @@
 package friendsofmine;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by walid on 19/10/2016.
  */
+
+@Entity
 public class Activite {
 
+    @Id
+    private Long id;
+
+    @NotNull
+    @NotBlank
     private String titre;
+
     private String descriptif;
-
-    public String getTitre() {
-        return this.titre;
-    }
-
-    public void setTitre (String t) {
-        if ((t == null) || (t == "")) {
-            this.titre = "unTitre";
-        } else {
-            this.titre = t;
-        }
-    }
 
 }
