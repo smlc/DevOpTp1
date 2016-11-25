4351b7ad0734fb1a172513226d412c095e2326f5
Alpha <sowoumar25@yahoo.fr>
2016-11-25 11:10:15 +0100

    Pass InscriptionTest.groovy

--- src/main/java/friendsofmine/Inscription.java
+++ src/main/java/friendsofmine/Inscription.java
@@ -1,7 +1,18 @@
 package friendsofmine;
 
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
 /**
  * Created by alpha on 25/11/2016.
  */
 public class Inscription {
+    @NotNull
+    private Utilisateur utilisateur;
+
+    @NotNull
+    private Activite activite;
+
+    @NotNull
+    private Date dateInscription;
 }
