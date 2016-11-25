42e269016661c6ef03a48c4fd43aae62dfe97ca1
Alpha <sowoumar25@yahoo.fr>
2016-11-25 11:58:16 +0100

    Pass InsciiptionRepositoryITest

--- src/main/java/friendsofmine/Inscription.java
+++ src/main/java/friendsofmine/Inscription.java
@@ -1,18 +1,32 @@
 package friendsofmine;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
 /**
  * Created by alpha on 25/11/2016.
  */
+@Entity
 public class Inscription {
+    @Id
+    @GeneratedValue
+    Long id;
+
     @NotNull
+    @OneToOne
     private Utilisateur utilisateur;
 
     @NotNull
+    @OneToOne
     private Activite activite;
 
     @NotNull
     private Date dateInscription;
+
+    @PrePersist
+    public void initCreationDate() {
+        dateInscription = new Date();
+    }
+
 }
--- src/main/java/friendsofmine/repositories/InscriptionRepository.groovy
+++ src/main/java/friendsofmine/repositories/InscriptionRepository.groovy
@@ -2,10 +2,13 @@ package friendsofmine.repositories
 
 import friendsofmine.Inscription
 import org.springframework.data.repository.PagingAndSortingRepository
+import org.springframework.stereotype.Repository
 
 /**
  * Created by alpha on 25/11/2016.
  */
+@Repository
 interface InscriptionRepository extends PagingAndSortingRepository<Inscription,Long> {
-
+    Inscription save(Inscription uneInscription);
+    Inscription findOne(Long id);
 }
