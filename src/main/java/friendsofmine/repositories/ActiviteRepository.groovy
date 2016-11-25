39dc6af574191c1bd82bebe8232e79157e90b70e
Alpha <sowoumar25@yahoo.fr>
2016-11-25 11:17:39 +0100

    Add class IncriptionRepositoryITest and other classes

--- src/main/java/friendsofmine/repositories/ActiviteRepository.groovy
+++ src/main/java/friendsofmine/repositories/ActiviteRepository.groovy
@@ -1,6 +1,7 @@
 package friendsofmine.repositories
 
 import friendsofmine.Activite
+import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
---
+++ src/main/java/friendsofmine/repositories/InscriptionRepository.java
@@ -0,0 +1,7 @@
+package friendsofmine.repositories;
+
+/**
+ * Created by alpha on 25/11/2016.
+ */
+public class InscriptionRepository {
+}
